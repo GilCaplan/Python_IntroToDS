e14cbf9bc3f0eabb5bc715ca94cf355fa13034c2
GilCaplan <rockycaplan@gmail.com>
2023-04-04 22:25:25 +0300

    solving q1, making variables to hold repeated values

--- main.py
+++ main.py
@@ -7,19 +7,27 @@ def solveq1(argv):
     data = d.load_data(argv[1], argv[2])
 
     summer = d.filter_by_feature(data, "season", data[1])
-
+    features = ["hum", "t1", "cnt"]
+    two_features = ["t1", "cnt"]
+    stat_funcs = [s.calc_mean, s.calc_stdv]
     print("Summer \n")
 
-    d.print_details(summer, ["hum", "t1", "cnt"], [s.calc_mean, s.calc_stdv])
+    d.print_details(summer, features, stat_funcs)
     print("\n")
-    d.print_joint_details(summer, ["t1", "cnt"], [s.calc_covariance])
+    d.print_joint_details(summer, two_features, [s.calc_covariance])
     print("\n")
 
     holiday = d.filter_by_feature(data, "holiday", data[1])
     print("Holiday: \n")
-    d.print_details(holiday, ["hum", "t1", "cnt"], [s.calc_mean, s.calc_stdv])
+    d.print_details(holiday, features, stat_funcs)
     print("\n")
-    d.print_joint_details(holiday, ["t1", "cnt"], [s.calc_covariance])
+    d.print_joint_details(holiday, two_features, [s.calc_covariance])
+    print("\n")
+
+    print("All: \n")
+    d.print_details(data, features, stat_funcs)
+    print("\n")
+    d.print_joint_details(data, two_features, [s.calc_covariance])
     print("\n")
 
 
