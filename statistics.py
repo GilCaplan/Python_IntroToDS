045cf04f1c1d23857833eddc4384739fe6330eeb
GilCaplan <rockycaplan@gmail.com>
2023-04-13 18:11:29 +0300

    shortened the function

--- statistics.py
+++ statistics.py
@@ -18,21 +18,19 @@ def calc_covariance(values1, values2):
     return sum(map(lambda x: (x[0] - mean1) * (x[1] - mean2), list(zip(values1, values2)))) / (len(values1) - 1)
 
 
-def population_statistics(feature_description, data, treatment, target, threshold, is_above, statistic_functions):
+def population_statistics(feature_desc, data, trt, trg, threshold, is_abv, stats_funcs):
     """
        Prints statistical information on filtered data above/under threshold of treatment,
        according to the target feature
-       :param feature_description: string which describes the feature
+       :param feature_desc: string which describes the feature
        :param data: dictionary where keys are the features and values are lists of values of the features
-       :param treatment: name of a feature
-       :param target: name of a feature
-       :param threshold: critical value of the treatment feature
-       :param is_above: indicates if we need rows that are above or under the threshold with respect to treatment
-       :param statistic_functions: list of statistical functions
+       :param trt: name of a feature, treatment
+       :param trg: name of a feature, target
+       :param threshold: critical value of the treatment feature, threshold
+       :param is_abv: indicates if we need rows that are above or under the threshold with respect to treatment, above
+       :param stats_funcs: list of statistical functions
        :return:
        """
-    values = {x for x in data[treatment] if (is_above and x > threshold) or (not is_above and x <= threshold)}
-    data1, data2 = filter_by_feature(data, treatment, values)
-
-    print(feature_description)
-    print_details(data1, [target], statistic_functions)
+    values = {x for x in data[trt] if (is_abv and x > threshold) or (not is_abv and x <= threshold)}
+    print(feature_desc)
+    print_details(filter_by_feature(data, trt, values)[0], [trg], stats_funcs)
