67d184321ebab0f4eedc1faacc97ee342d3cbb87
GilCaplan <rockycaplan@gmail.com>
2023-05-16 23:17:05 +0300

    kk

---
+++ clustering.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+np.random.seed(2)
+
+
+def add_noise(data):
+    """
+    :param data: dataset as numpy array of shape (n, 2)
+    :return: data + noise, where noise~N(0,0.001^2)
+    """
+    noise = np.random.normal(loc=0, scale=0.001, size=data.shape)
+    return data + noise
+
+
+def choose_initial_centroids(data, k):
+    """
+    :param data: dataset as numpy array of shape (n, 2)
+    :param k: number of clusters
+    :return: numpy array of k random items from dataset
+    """
+    n = data.shape[0]
+    indices = np.random.choice(range(n), k, replace=False)
+    return data[indices]
+
+
+# ====================
+def transform_data(df, features):
+    """
+    Performs the following transformations on df:
+        - selecting relevant features
+        - scaling
+        - adding noise
+    :param df: dataframe as was read from the original csv.
+    :param features: list of 2 features from the dataframe
+    :return: transformed data as numpy array of shape (n, 2)
+    """
+    pass
+    # return data
+
+
+def kmeans(data, k):
+    """
+    Running kmeans clustering algorithm.
+    :param data: numpy array of shape (n, 2)
+    :param k: desired number of cluster
+    :return:
+    * labels - numpy array of size n, where each entry is the predicted label (cluster number)
+    * centroids - numpy array of shape (k, 2), centroid for each cluster.
+    """
+    pass
+    # return labels, centroids
+
+
+def visualize_results(data, labels, centroids, path):
+    """
+    Visualizing results of the kmeans model, and saving the figure.
+    :param data: data as numpy array of shape (n, 2)
+    :param labels: the final labels of kmeans, as numpy array of size n
+    :param centroids: the final centroids of kmeans, as numpy array of shape (k, 2)
+    :param path: path to save the figure to.
+    """
+    pass
+    # plt.savefig(path)
+
+
+def dist(x, y):
+    """
+    Euclidean distance between vectors x, y
+    :param x: numpy array of size n
+    :param y: numpy array of size n
+    :return: the euclidean distance
+    """
+    pass
+    # return distance
+
+
+def assign_to_clusters(data, centroids):
+    """
+    Assign each data point to a cluster based on current centroids
+    :param data: data as numpy array of shape (n, 2)
+    :param centroids: current centroids as numpy array of shape (k, 2)
+    :return: numpy array of size n
+    """
+    pass
+    # return labels
+
+
+def recompute_centroids(data, labels, k):
+    """
+    Recomputes new centroids based on the current assignment
+    :param data: data as numpy array of shape (n, 2)
+    :param labels: current assignments to clusters for each data point, as numpy array of size n
+    :param k: number of clusters
+    :return: numpy array of shape (k, 2)
+    """
+    pass
+    # return centroids
+
