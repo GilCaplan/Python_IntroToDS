e98a0a75595f3f7363be01ae381fb8d878458520
GilCaplan <rockycaplan@gmail.com>
2023-05-17 03:20:34 +0300

    starting project, part A 1,2,3 should be done.

--- data.py
+++ data.py
@@ -8,7 +8,11 @@ def load_data(path):
 
 def add_new_columns(df):
     """adds columns to df and returns the new df"""
-    df['season_name'].apply(lambda x: ['spring', 'summer', 'fall', 'winter'][x])
+    df['season_name'] = df['season'].apply(lambda x: ['spring', 'summer', 'fall', 'winter'][x])
 
+    df['Hour'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").hour)# 0-23
+    df['Day'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").day)# 1-31
+    df['Month'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").month)# 1-12
+    df['Year'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").year)# 2015-17
     return df
 
