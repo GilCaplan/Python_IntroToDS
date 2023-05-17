08e9b6bb5fe5b7e514624fd2d955491658b7d7d6
GilCaplan <rockycaplan@gmail.com>
2023-05-17 03:29:42 +0300

    starting project, part A 1,2,3,5 should be done.

--- data.py
+++ data.py
@@ -8,11 +8,18 @@ def load_data(path):
 
 def add_new_columns(df):
     """adds columns to df and returns the new df"""
+    # 2 Gil
     df['season_name'] = df['season'].apply(lambda x: ['spring', 'summer', 'fall', 'winter'][x])
 
+    # 3 Gil
     df['Hour'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").hour)# 0-23
     df['Day'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").day)# 1-31
     df['Month'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").month)# 1-12
     df['Year'] = df['timestamp'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M:%S").year)# 2015-17
+
+    # 4 סייבה
+
+    # 5 Gil
+    df['t_diff'] = df.apply(lambda r: r['t1'] - r['t2'])
     return df
 
