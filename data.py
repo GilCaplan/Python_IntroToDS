876f5a9539599260595ef1f431b8995866286b3c
GilCaplan <rockycaplan@gmail.com>
2023-05-18 16:49:43 +0300

    starting project, part A 1,2,3,5,7 should be done. debugged and fixed these (think i did 7 right but need to check)
    started on 7.1 made corr_df which works
    
    B started on 1

--- data.py
+++ data.py
@@ -29,16 +29,29 @@ def data_analysis(df):
     """prints statistics on transformed df"""
 
     dic = df.to_dict(orient="list")
-
-
     # 6 סייבה
 
     # 7
     # 1. Gil, 5 features (different from each other) that have the highest absolute correlation
 
-    features = df.columns.values
-    corr_df = df.drop(columns=['timestamp','season_name']).corr()
-    dic = {f1+" "+f2:  corr_df.loc(f1, f2) for f1, f2 in zip(corr_df.columns.values, corr_df.rows.values) if f1!=f2}
+    corr_df = df.drop(columns=['timestamp', 'season_name']).corr()
+    # gets correlations of columns and put's it in a new df
+    features = corr_df.columns.values
+    corr_dic = {}
+    for i, feature in enumerate(features):
+        for x in range(i):
+            corr_dic[str(feature) + " " + str(features[x])] = abs(corr_df[feature][features[x]])
+
+    # made dictionary with all correlation values without repeating features, now need to find top 5
+    features_sorted = list(sorted(corr_dic.keys(), key=lambda x: corr_dic[x], reverse=True))
+    print("max vals: ")
+    for i in range(5):
+        print(features_sorted[i] + " " + "%.2f" % corr_dic[features_sorted[i]])
+    print("min vals")
+    for i in range(5):
+        j = len(corr_dic) - i - 1
+        print(features_sorted[j] + " " + "%.2f" % corr_dic[features_sorted[j]])
+
     # make dictionary with all feature corr values, need to fix cause doesn't work
     # 2. uhhhh, 5 features (different from each other) that have the lowest absolute correlation
     return dic
