fba61df52e7409ca12d57a093040f831d5787fc8
lwwws <yumifan3679@yahoo.com>
2023-05-19 20:27:27 +0300

    added some of my parts

--- data.py
+++ data.py
@@ -20,6 +20,7 @@ def add_new_columns(df):
     df['Year'] = df['timestamp'].apply(lambda x: get_time(x).year)
 
     # 4 סייבה
+    df['is_weekend_holiday'] = df.apply(lambda row: 2 * row['is_holiday'] + row['is_weekend'], axis=1)
 
     # 5 Gil
     df['t_diff'] = df.apply(lambda row: row['t1'] - row['t2'], axis=1)
@@ -35,6 +36,13 @@ def data_analysis(df):
 
     dic = df.to_dict(orient="list")
     # 6 סייבה
+    print("describe output:")
+    print(df.describe().to_string())
+    print()
+    print("corr output:")
+    corr = df.corr()
+    print(corr.to_string())
+    print()
 
     # 7
     # Gil, 5 features (different from each other) that have the highest/lowest absolute correlation
@@ -42,6 +50,7 @@ def data_analysis(df):
     # gets correlations of columns and put's it in a new df
     features = corr_df.columns.values
     y = lambda feature, x: abs(corr_df[feature][features[x]])
+    # I think in recommendation they meant to make (feature, features[x]) ?
     key = lambda feature, x: f'({feature}, {features[x]})'
     corr_dic = {key(feature, x): y(feature, x) for i, feature in enumerate(features) for x in range(i)}
 
@@ -54,11 +63,19 @@ def data_analysis(df):
     features_sorted = list(sorted(corr_dic.keys(), key=lambda x: corr_dic[x], reverse=True))
     print("Highest correlated are:")
     for i in range(5):
-        print(features_sorted[i] + " with " + "%.2f" % corr_dic[features_sorted[i]])
+        print(features_sorted[i] + " with " + "%.6f" % corr_dic[features_sorted[i]])
     print("Lowest correlated are:")
     for i in range(5):
         j = len(corr_dic) - i - 1
-        print(features_sorted[j] + " with " + "%.2f" % corr_dic[features_sorted[j]])
+        print(features_sorted[j] + " with " + "%.6f" % corr_dic[features_sorted[j]])
 
     # 8. uhhhh
-    return dic
+
+    seasons = df.groupby('season_name')
+
+    for season in seasons:
+        print(season[0] + " average t_diff is " + "%.2f" % season[1]['t_diff'].mean())
+
+    print("All average t_diff is " + "%.2f" % df['t_diff'].mean())
+
+    return dic # function needs to return something?
